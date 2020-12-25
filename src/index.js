513b4bdfcc6a9773d9278b5aafe8708f31579d62
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-25 16:42:25 -0300

    feat: imports necessarios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { GlobalStyle } from './globalStyle';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.Fragment>
+      <GlobalStyle />
+      <App />
+    </React.Fragment>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
