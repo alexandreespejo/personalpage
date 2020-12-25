c9f2e3da3363fa36ba11d65e16f0aebabe20e027
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-25 16:41:51 -0300

    feat: criando rotas com react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import Main from './pages/main';
+import About from './pages/about';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+import { Switch, Route } from 'react-router-dom';
 function App() {
-  return <Main />;
+  return (
+    <Switch>
+      <Route path="/" exact component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
+  );
 }
 export default App;
