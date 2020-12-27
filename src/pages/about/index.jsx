3c08e97e1b28d0b613d98aa005b9c579afbedd06
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-27 18:06:04 -0300

    add: pagina about

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,15 @@
+import Main from '../main';
+import Icon from '../../components/ProfileIcon';
+const About = () => {
+  return (
+    <Main>
+      <Icon>
+        {
+          'https://avatars1.githubusercontent.com/u/62163105?s=460&u=5b60edb54072941138b7861e3acefd621c01a039&v=4'
+        }
+      </Icon>
+    </Main>
+  );
+};
+
+export default About;
