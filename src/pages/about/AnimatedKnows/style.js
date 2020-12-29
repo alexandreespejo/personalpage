9f095afa83e9f7fd6ae6a5b25d08769edca5c8ee
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-29 20:23:04 -0300

    add: componente de animação dos icones

---
+++ src/pages/about/AnimatedKnows/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Knowledges = styled.div`
+  display: flex;
+  align-self: center;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  width: 280px;
+  height: 300px;
+  flex-wrap: wrap;
+  border-radius: 20px;
+  background: rgba(51, 49, 56, 0.35);
+`;
+export const KnowCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 33%;
+  height: 33%;
+  border-radius: 10px;
+  /* background: ${(props) => props.theme.primary}; */
+`;
