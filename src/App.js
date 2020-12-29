0314dc06831b50fd6b3f866285228c1bb96e52e0
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-29 20:17:50 -0300

    feat: importação e utilização do context tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { TabProvider } from './contexts/tab';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
 import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={About} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/contact" component={Contact} />
-    </Switch>
+    <TabProvider>
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </TabProvider>
   );
 }
 export default App;
