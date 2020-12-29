3d8e6cc7b4cbb54d9bebadd5988b3dcf11180c01
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-28 23:38:22 -0300

    add: estilos da pagina sobre

---
+++ src/pages/about/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Content = styled.main`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const LeftContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+`;
+export const RightContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+`;
