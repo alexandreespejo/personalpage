ad6ead70082d5766063167df888fcdb39622c3be
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-28 23:40:41 -0300

    refactor: utilizando nova paleta e valores de propriedades

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -2,23 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
   display: ${(props) => props.display || 'flex'};
+  position: ${(props) => props.position || 'none'};
   align-items: center;
   justify-content: center;
-  width: ${(props) => props.width || '33%'};
-  height: ${(props) => props.height || '99%'};
+  width: ${(props) => props.width || '140px'};
+  height: ${(props) => props.height || '40px'};
   margin-top: 1px;
   background: transparent;
   cursor: pointer;
   border: none;
   outline: none;
-  transition: all 0.2s linear;
+  transition: all 0.5s linear;
   transform: ${(props) => props.invert && 'rotateZ(180deg)'};
   :hover {
     /* border-bottom: 2px solid ${(props) => props.theme.title}; */
-    color: ${(props) => props.theme.title};
+    color: ${(props) => props.theme.colorHover};
   }
   text-decoration: none;
   font-size: ${(props) => props.fontSize || '18px'};
   font-weight: bold;
-  color: ${(props) => props.theme.ligth};
+  color: ${(props) => props.theme.colorButton};
 `;
