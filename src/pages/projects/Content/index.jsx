fec03d487efb6eb3d1951764f221361c5557fbc1
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-30 20:47:20 -0300

    fix: tamanho do elemento

--- src/pages/projects/Content/index.jsx
+++ src/pages/projects/Content/index.jsx
@@ -1,17 +1,17 @@
 import { useTransition } from 'react-spring';
-import { Main } from './style';
+import { Main, Title } from './style';
 const Content = ({ contentList, active }) => {
   const transitions = useTransition(active, (p) => p, {
-    from: { opacity: 0, width: '0%', height: '0%' },
-    enter: { opacity: 1, width: '80%', height: '60%' },
-    leave: { opacity: 0, width: '0%', height: '0%' },
+    from: { opacity: 0, width: 0, height: 0 },
+    enter: { opacity: 1, width: 1200, height: 500 },
+    leave: { opacity: 0, width: 0, height: 0 },
   });
   return (
     <>
       {transitions.map(({ item, props, key }) => {
         return (
           <Main key={key} {...props} style={props}>
-            {contentList[item]}
+            <Title>{contentList[item]}</Title>
           </Main>
         );
       })}
