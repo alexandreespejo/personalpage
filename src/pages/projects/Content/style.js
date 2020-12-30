38914aad9823e3352fcef92cb8bd74ef8865ade4
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-30 20:47:52 -0300

    fix: props das estilizacoes

--- src/pages/projects/Content/style.js
+++ src/pages/projects/Content/style.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 import { animated } from 'react-spring';
+
 export const Main = styled(animated.main)`
   /* display: ${(props) => (props.isActive ? 'flex' : 'none')}; */
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 80%;
-  height: 60%;
+  margin-top: -60px;
   border-radius: 20px;
   background: ${(props) => props.theme.secondary};
+`;
+
+export const Title = styled.label`
+  font-size: 30px;
 `;
