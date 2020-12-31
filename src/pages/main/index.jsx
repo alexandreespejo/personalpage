7eb816b70cbdebcbe20bfe0467ec87c6062f3de4
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-31 13:34:33 -0300

    refactor: removido variavel e componente desnecessario

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,20 +2,12 @@ import { Mother, Content } from './style';
 import { ThemeProvider } from 'styled-components';
 import Header from '../../components/Header';
 function Main({ children }) {
-  var dark = {
-    primary: 'rgb(26, 26, 29)',
-    secondary: 'rgba(51, 49, 56, 0.87)',
-    colorHover: '#00BFA6',
-    colorButton: 'rgba(250, 250, 250, 0.87)',
-  };
   window.addEventListener('click', () => {});
   return (
-    <ThemeProvider theme={dark}>
-      <Mother>
-        <Header>{['Sobre', 'Projetos', 'Contato']}</Header>
-        <Content>{children}</Content>
-      </Mother>
-    </ThemeProvider>
+    <Mother>
+      <Header>{['Sobre', 'Projetos', 'Contato']}</Header>
+      <Content>{children}</Content>
+    </Mother>
   );
 }
 export default Main;
