b0167adfa6b0cdd152be999d59308d3fa5be3b0b
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-31 13:28:52 -0300

    feat: transformado paleta de cores em variaveis

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,5 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
+  :root{
+    --page-primary:rgb(26, 26, 29);
+    --page-secondary:rgba(51, 49, 56, 0.87);
+    --text-primary:whitesmoke;
+    --text-secondary:rgba(250, 250, 250, 0.87);
+    --button-primary:rgba(0, 191, 166,0.87);
+    --button-secondary:rgb(0, 210, 180);
+  }
   body {
     margin: 0;
     padding: 0;
