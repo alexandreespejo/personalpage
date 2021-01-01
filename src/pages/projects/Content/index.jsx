bb559ca604e2649ebdb179e1ff75e8054cd04586
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-31 21:56:26 -0300

    feat: posicionando components na grid

--- src/pages/projects/Content/index.jsx
+++ src/pages/projects/Content/index.jsx
@@ -1,9 +1,9 @@
 import { useTransition } from 'react-spring';
-import { Main, Title } from './style';
+import { Main, Title, Text, Link, Image } from './style';
 const Content = ({ contentList, active }) => {
   const transitions = useTransition(active, (p) => p, {
     from: { opacity: 0, width: 0, height: 0 },
-    enter: { opacity: 1, width: 1200, height: 500 },
+    enter: { opacity: 1, width: 1000, height: 400 },
     leave: { opacity: 0, width: 0, height: 0 },
   });
   return (
@@ -11,7 +11,20 @@ const Content = ({ contentList, active }) => {
       {transitions.map(({ item, props, key }) => {
         return (
           <Main key={key} {...props} style={props}>
-            <Title>{contentList[item]}</Title>
+            <Title>
+              <h2>{contentList[item]?.name}</h2>
+            </Title>
+            <Text>
+              <p>{contentList[item]?.descripition}</p>
+            </Text>
+            <Link>
+              {contentList[item].links?.map((link) => {
+                return <a href={link.src}>{link.name}</a>;
+              })}
+            </Link>
+            <Image>
+              <img src={contentList[item]?.picture} alt="pic" />
+            </Image>
           </Main>
         );
       })}
