0b7b3690399c040e9fa55f17f0d38b6a26464cd6
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-31 21:53:18 -0300

    refactor: melhorando contraste da paleta

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -3,8 +3,8 @@ export const GlobalStyle = createGlobalStyle`
   :root{
     --page-primary:rgb(26, 26, 29);
     --page-secondary:rgba(51, 49, 56, 0.87);
-    --text-primary:whitesmoke;
-    --text-secondary:rgba(250, 250, 250, 0.87);
+    --text-primary:rgba(250, 250, 250, 0.87);
+    --text-secondary:#b3ab9a;
     --button-primary:rgba(0, 191, 166,0.87);
     --button-secondary:rgb(0, 210, 180);
   }
