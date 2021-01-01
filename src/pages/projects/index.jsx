58399cac24c5aa3e9a7d34bd427f00505648fe2a
alexandreespejo <alexandreespejo98@gmail.com>
2020-12-31 21:55:47 -0300

    feat: estrutura da listagem de projetos

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,10 +1,24 @@
 import { useContext } from 'react';
 import { TabContext } from '../../contexts/tab';
+import pic1 from '../../assets/photo1.png';
 import Main from '../main';
 import Footer from './Footer';
 import Content from './Content';
 const Projects = () => {
-  const content = ['Em Breve', 'projeto2', 'projeto3'];
+  const content = [
+    {
+      name: 'Portifólio Web',
+      descripition:
+        'Dolor id velit id ullamco sit. Exercitation quis ea duis deserunt occaecat do enim fugiat laborum reprehenderit. Exercitation mollit laborum esse Lorem veniam quis.',
+      picture: `${pic1}`,
+      links: [
+        { name: 'aaa', src: 'google.com' },
+        { name: 'bbb', src: 'youtube.com' },
+      ],
+    },
+    { name: 'projeto2' },
+    { name: 'projeto3' },
+  ];
   const { activeTab, setActiveTab } = useContext(TabContext);
   return (
     <Main>
