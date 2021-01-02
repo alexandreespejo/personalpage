727598508fdf7b3a53fd93d75520bafc2a0227b5
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-02 18:26:11 -0300

    feat: adicionado mais cores a paleta

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root{
-    --page-primary:rgb(26, 26, 29);
-    --page-secondary:rgba(51, 49, 56, 0.87);
+    --page-primary:rgb(26, 26, 26);
+    --page-primary-light:#212121;
+    --page-secondary:rgba(51, 49, 56, 0.70);
     --text-primary:rgba(250, 250, 250, 0.87);
     --text-secondary:#b3ab9a;
     --button-primary:rgba(0, 191, 166,0.87);
