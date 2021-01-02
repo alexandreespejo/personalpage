5cc3ee84ceb5b0aba4cb8ec1291dfcb5e0db2c96
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-02 18:34:15 -0300

    feat: utilizando buttons e distribuindo informação do objeto

--- src/pages/projects/Content/index.jsx
+++ src/pages/projects/Content/index.jsx
@@ -1,29 +1,47 @@
-import { useTransition } from 'react-spring';
+import { useTransition, useSpring, animated } from 'react-spring';
 import { Main, Title, Text, Link, Image } from './style';
+import Button from '../../../components/Button';
+import { AiOutlineLink, BiMeteor } from 'react-icons/all';
 const Content = ({ contentList, active }) => {
   const transitions = useTransition(active, (p) => p, {
     from: { opacity: 0, width: 0, height: 0 },
     enter: { opacity: 1, width: 1000, height: 400 },
     leave: { opacity: 0, width: 0, height: 0 },
   });
+
   return (
     <>
       {transitions.map(({ item, props, key }) => {
         return (
           <Main key={key} {...props} style={props}>
             <Title>
-              <h2>{contentList[item]?.name}</h2>
+              <h2>
+                {contentList[item]?.name + '  '}
+                <BiMeteor />
+              </h2>
             </Title>
-            <Text>
-              <p>{contentList[item]?.descripition}</p>
-            </Text>
+
             <Link>
               {contentList[item].links?.map((link) => {
-                return <a href={link.src}>{link.name}</a>;
+                return (
+                  <Button
+                    as="a"
+                    href={link.src}
+                    colorHover={true}
+                    active={true}
+                    color={' var(--text-primary)'}
+                  >
+                    {<AiOutlineLink />}
+                    {link.name + ' '}
+                  </Button>
+                );
               })}
             </Link>
             <Image>
-              <img src={contentList[item]?.picture} alt="pic" />
+              <img src={contentList[item]?.picture} />
+              <Text>
+                <p>{contentList[item]?.descripition}</p>
+              </Text>
             </Image>
           </Main>
         );
