50fd0ec41bd2e6944cd75c4296e837579dde65ac
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-02 18:31:07 -0300

    refactor: mudado tipo de componente e estilização

--- src/pages/about/Text/style.js
+++ src/pages/about/Text/style.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-export const Text = styled.p`
+export const Text = styled.label`
   align-self: center;
   text-align: justify;
-  margin: ${(props) => props.fontMargin};
   font-size: ${(props) => props.fontSize};
+  margin: ${(props) => props.margin};
+  opacity: ${(props) => props.opacity};
   color: ${(props) => props.fontColor};
+  max-width: 450px;
 `;
