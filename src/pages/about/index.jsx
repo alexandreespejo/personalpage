2a607b726d0dd6a228fa8981ce454d60a42b8f97
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-02 18:29:00 -0300

    feat: usando spring para animar titulos

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,15 @@
 import Main from '../main';
 import Icon from '../../components/ProfileIcon';
+import { useSpring, animated } from 'react-spring';
 import { RightContent, LeftContent, Content } from './style';
 import Knowledges from './AnimatedKnows';
 import Label from './Text';
 const About = () => {
+  const animation = useSpring({
+    from: { opacity: 0, transform: 'translate3d(0,200px,0)' },
+    to: { opacity: 1, transform: 'translate3d(0,0,0)' },
+    config: { delay: 300 },
+  });
   const knowList = [
     {
       src: 'https://img.icons8.com/color/48/000000/react-native.png',
@@ -48,44 +54,28 @@ const About = () => {
     <Main>
       <Content>
         <LeftContent>
-          <Label fontSize={'40px'} fontColor={'var(--text-primary)'}>
-            Alexandre Espejo
+          <animated.h1 style={animation}>Alexandre Espejo</animated.h1>
+          <Label fontSize={'20px'} fontColor={'rgba(179, 171, 154,0.5)'}>
+            Desenvolvedor Full-Stack jr.
           </Label>
           <Label
             fontSize={'18px'}
-            fontMargin={'0 0 0 10px'}
             fontColor={'var(--text-secondary)'}
+            margin={'50px 0px 0px 0px '}
           >
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mollit culpa exercitation officia ut
-            aliquip nostrud pariatur velit elit ipsum. In anim ipsum do
-            reprehenderit do in fugiat. Excepteur laboris et ex reprehenderit
-            mollit sint dolore aliqua voluptate minim. Aliquip dolore
-            adipisicing excepteur exercitation et dolore anim Pariatur in anim
-            pariatur sunt. Proident irure dolor anim tempor veniam nostrud anim
-            pariatur excepteur exercitation tempor. Dolor et aute adipisicing
-            enim magna ea enim laborum eu voluptate cillum cillum exercitation
-            pariatur. Adipisicing exercitation laboris nulla anim. Mollit nulla
-            dolore eu sint adipisicing aliquip duis ad reprehenderit incididunt
-            nulla qui aliqua. Consectetur dolor veniam minim incididunt ex
-            ullamco non cupidatat nisi deserunt do adipisicing ad Amet ipsum
-            deserunt excepteur labore dolor minim. Ut commodo duis aliquip
-            voluptate eu nostrud officia cupidatat. Est tempor voluptate ipsum
-            irure ad in eu commodo et excepteur est amet. Dolore fugiat
-            incididunt voluptate reprehenderit non esse. Fugiat cupidatat in
-            aliquip anim. Laboris incididunt ullamco quis fugiat reprehenderit
-            cillum auteerunt excepteur labore dolor minim. Ut commodo duis
-            aliquip voluptate eu nostrud officia cupidatat. Est tempor voluptate
-            ipsum irure ad in eu commodo et excepteur est amet. Dolore fugiat
-            incididunt voluptate reprehenderit non esse. Fugiat cupidatat in
-            aliquip anim. Laboris incididunt ullamco quis fugiat reprehenderit
-            cillum aute.
+            Olá, sou o <strong>Alexandre Espejo</strong> um desenvolvedor de
+            Bauru-SP que a pouco tempo está desbravando esse vasto universo da
+            programação.Apaixonado por <strong>JavaScript</strong>, às
+            tecnologias que mais gosto de estudar e trabalhar são{' '}
+            <strong>Node.js, React.js e React Native</strong>, porem sempre
+            estou aberto a me aventurar em novas tecnologias.
           </Label>
         </LeftContent>
         <RightContent>
           <Icon iconWidth={'200px'} children={img} />
           <Label
             fontSize={'20px'}
-            fontMargin={'20px 0 0 0'}
+            margin={'15px 0px 0px 0px'}
             fontColor={'var(--text-primary)'}
           >
             Principais habilidades :
