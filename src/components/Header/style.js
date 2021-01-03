1b85eedab2fd246f7f8f752a1b96a0136e06f83d
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-03 19:42:21 -0300

    fix: componente não alinhava conteudo

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,22 +1,19 @@
 import styled from 'styled-components';
-import { animated } from 'react-spring';
 
-export const Container = styled(animated.header)`
+export const Container = styled.header`
+  background-color: var(--page-secondary);
+  height: 60px;
+  width: 100%;
+  transform: ${(props) =>
+    props.isShowMenu ? 'translate3d(0,0,0)' : 'translate3d(-100%,0,0)'};
+  transition: transform 0.6s linear;
+`;
+
+export const Division = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  background-color: var(--page-secondary);
-  justify-content: ${(props) => (props.showMenu ? 'space-between' : 'start')};
-  height: 60px;
-  width: 100%;
-  transition: all 0.25s linear;
-`;
-
-export const Division = styled(animated.div)`
-  display: ${(props) => (props.show ? 'flex' : 'none')};
-  flex-direction: row;
   justify-content: space-around;
-  align-items: center;
   width: 100%;
-  height: 100%;
+  height: 60px;
 `;
