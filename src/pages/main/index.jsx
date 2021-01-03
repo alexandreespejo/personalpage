01649608f6cfbb87a85027e6c17b10bf2afc17e1
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-03 19:43:51 -0300

    feat: inserindo pages na principal

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,13 +1,25 @@
+import { useContext } from 'react';
+import { TabContext } from '../../contexts/tab';
 import { Mother, Content } from './style';
-import { ThemeProvider } from 'styled-components';
 import Header from '../../components/Header';
-function Main({ children }) {
-  window.addEventListener('click', () => {});
+import About from '../about';
+import Projects from '../projects';
+import Contact from '../contact';
+const Main = () => {
+  const { activePage } = useContext(TabContext);
   return (
     <Mother>
-      <Header>{['Sobre', 'Projetos', 'Contato']}</Header>
-      <Content>{children}</Content>
+      <Header />
+      <Content show={activePage === 0}>
+        <About />
+      </Content>
+      <Content show={activePage === 1}>
+        <Projects />
+      </Content>
+      <Content show={activePage === 2}>
+        <Contact />
+      </Content>
     </Mother>
   );
-}
+};
 export default Main;
