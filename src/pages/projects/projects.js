3d196ab61ffa61b1f1176f9fe875e7ded25fea1a
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-03 10:54:20 -0300

    add: dados de projetos para listar

---
+++ src/pages/projects/projects.js
@@ -0,0 +1,16 @@
+import pic1 from '../../assets/photo1.png';
+export const data = [
+  {
+    name: 'Portifólio Web',
+    descripition:
+      'Dolor id velit id ullamco sit. Exercitation quis ea duis deserunt occaecat do enim fugiat laborum reprehenderit. Exercitation mollit laborum esse Lorem veniam quis.',
+    picture: `${pic1}`,
+    links: [
+      {
+        name: 'Repositório',
+        src: 'https://github.com/alexandreespejo/personalpage',
+      },
+    ],
+  },
+  { name: 'Em Breve' },
+];
