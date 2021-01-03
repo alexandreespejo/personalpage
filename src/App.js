4d78dd94f6cb6efcefe58b887900db2895bed962
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-03 19:40:58 -0300

    feat: removendo route e componentes do router dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import { TabProvider } from './contexts/tab';
 import Main from './pages/main';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Contact from './pages/contact';
-import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <TabProvider>
-      <Switch>
-        <Main>
-          <Route path="/personalpage/" exact component={About} />
-          <Route path="/personalpage/projects" exact component={Projects} />
-          <Route path="/personalpage/contact" exactcomponent={Contact} />
-        </Main>
-      </Switch>
+      <Main />
     </TabProvider>
   );
 }
