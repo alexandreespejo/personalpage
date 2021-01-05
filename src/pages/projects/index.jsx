9290809ea32bd3389e5840160acf1c5418fbb5d9
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-05 19:36:33 -0300

    refactor: renomeando varivel e import

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,13 +2,13 @@ import { useContext } from 'react';
 import { TabContext } from '../../contexts/tab';
 import Footer from './Footer';
 import Content from './Content';
-import { data } from './projects';
+import { contentList } from './projectData';
 const Projects = () => {
   const { activeTab, setActiveTab } = useContext(TabContext);
   return (
     <>
-      <Content contentList={data} active={activeTab} />
-      <Footer contentList={data} active={activeTab} set={setActiveTab} />
+      <Content contentList={contentList} active={activeTab} />
+      <Footer contentList={contentList} active={activeTab} set={setActiveTab} />
     </>
   );
 };
