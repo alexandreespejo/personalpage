a42aa113143c82944c09c6f48c425aab18424a42
alexandreespejo <alexandreespejo98@gmail.com>
2021-01-07 09:42:21 -0300

    refactor: retirado router-dom e importado estilo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { GlobalStyle } from './globalStyle';
+import { GlobalStyle } from './styles/global';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.Fragment>
-      <GlobalStyle />
-      <App />
-    </React.Fragment>
-  </BrowserRouter>,
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
   document.getElementById('root')
 );
