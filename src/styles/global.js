3b0e2305005871af1d1ff80c336b2aef4dcf9ac2
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:23:06 -0300

    refactor: color update

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root{
-    --page-primary:rgb(26, 26, 26);
+    --page-primary: #1a1a1a;
     --page-primary-light:#212121;
     --page-secondary:rgba(51, 49, 56, 0.70);
-    --text-primary:rgba(250, 250, 250, 0.87);
-    --text-secondary:#b3ab9a;
+    --text-primary:rgba(250, 250, 250, 0.75);
+    --text-secondary:rgba(250, 250, 250, 0.4);
     --button-primary:rgba(0, 191, 166,0.87);
     --button-secondary:rgb(0, 210, 180);
   }
   body {
-    font-family:sans-serif;
+    font-family:'Roboto',sans-serif;
     margin: 0;
     padding: 0;
-    color: whitesmoke;
+    color: var(--text-primary);
   }
 `;
