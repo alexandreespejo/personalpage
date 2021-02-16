eb7d68a78fb2120ccdd13724e98cf5561e0c96e6
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:20:21 -0300

    refactor: unnecessary imports remotion

--- src/pages/contact/starsRain/index.jsx
+++ src/pages/contact/starsRain/index.jsx
@@ -1,12 +1,8 @@
-import { useContext } from 'react';
-import { TabContext } from '../../../contexts/tab';
 import { Stars, Star } from '../../../styles/starsRain';
 const StarsRain = () => {
-  const { experience, setExperience } = useContext(TabContext);
   const starClick = (component) => {
     component.target.offsetParent.style.opacity = 0.3;
     component.target.offsetParent.style.pointerEvents = 'none';
-    setExperience(experience + 100);
 
     setTimeout(() => {
       component.target.offsetParent.style.opacity = 1;
