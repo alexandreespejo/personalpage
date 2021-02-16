f43a7f6ad6c6b813c64bd49e9e0cd3b5e1ca19a6
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:11:05 -0300

    feat: using new style

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,41 +1,20 @@
-import Icon from '../../components/ProfileIcon';
-import { useSpring, animated } from 'react-spring';
-import { RightContent, LeftContent, Content } from '../../styles/about';
-import { aboutContent } from './aboutData';
-import Knowledges from './AnimatedKnows';
-import Label from './Text';
+import HardSkills from './hardSkills';
+import SoftSkill from './softSkills';
+import { Content, Division } from '../../styles/about';
+import Card from './card';
 const About = () => {
-  const animation = useSpring({
-    from: { opacity: 0, transform: 'translate3d(0,200px,0)' },
-    to: { opacity: 1, transform: 'translate3d(0,0,0)' },
-    config: { delay: 300 },
-  });
   return (
     <Content>
-      <LeftContent>
-        <animated.h1 style={animation}>{aboutContent.title}</animated.h1>
-        <Label fontSize={'20px'} fontColor={'rgba(179, 171, 154,0.5)'}>
-          {aboutContent.descripition}
-        </Label>
-        <Label
-          fontSize={'18px'}
-          fontColor={'var(--text-secondary)'}
-          margin={'50px 0px 0px 0px '}
-        >
-          {aboutContent.text}
-        </Label>
-      </LeftContent>
-      <RightContent>
-        <Icon iconWidth={'200px'} children={aboutContent.image} />
-        <Label
-          fontSize={'20px'}
-          margin={'15px 0px 0px 0px'}
-          fontColor={'var(--text-primary)'}
-        >
-          Principais habilidades :
-        </Label>
-        <Knowledges />
-      </RightContent>
+      <Division>
+        <HardSkills />
+      </Division>
+      <Division>
+        <SoftSkill />
+      </Division>
+
+      <Division>
+        <Card />
+      </Division>
     </Content>
   );
 };
