72c06b5687f6c3044db8ff32e9ba5e8320ff0532
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:10:23 -0300

    feat: new context variables

--- src/contexts/tab.jsx
+++ src/contexts/tab.jsx
@@ -2,6 +2,8 @@ import { createContext, useEffect, useState } from 'react';
 const TabContext = createContext();
 const TabProvider = ({ children }) => {
   const [activeTab, setActiveTab] = useState(0);
+  const [experience, setExperience] = useState(100);
+  const [level, setLevel] = useState(1);
   const [activePage, setActivePage] = useState(
     sessionStorage.getItem('currentyPage') === null
       ? 0
@@ -10,9 +12,25 @@ const TabProvider = ({ children }) => {
   useEffect(() => {
     sessionStorage.setItem('currentyPage', activePage);
   }, [activePage]);
+  useEffect(() => {
+    if (experience >= 1000) {
+      setExperience(0);
+      setLevel(level + 1);
+    }
+  }, [experience, level]);
+
   return (
     <TabContext.Provider
-      value={{ activeTab, setActiveTab, activePage, setActivePage }}
+      value={{
+        activeTab,
+        setActiveTab,
+        activePage,
+        setActivePage,
+        experience,
+        setExperience,
+        level,
+        setLevel,
+      }}
     >
       {children}
     </TabContext.Provider>
