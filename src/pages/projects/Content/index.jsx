529c0bb4b6bab68f74c592425208e7c0e2750dc6
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:24:24 -0300

    feat: using project data

--- src/pages/projects/Content/index.jsx
+++ src/pages/projects/Content/index.jsx
@@ -1,53 +1,41 @@
-import { useTransition } from 'react-spring';
 import { Main, Title, Text, Link, Image } from '../../../styles/projectContent';
 import Button from '../../../components/Button';
-import { AiOutlineLink, BiMeteor } from 'react-icons/all';
+import { AiOutlineLink, GiStarsStack } from 'react-icons/all';
 const Content = ({ contentList, active }) => {
-  const transitions = useTransition(active, (p) => p, {
-    from: { opacity: 0, width: 0, height: 0 },
-    enter: { opacity: 1, width: 1000, height: 400 },
-    leave: { opacity: 0, width: 0, height: 0 },
-  });
-
   return (
     <>
-      {transitions.map(({ item, props, key }) => {
-        return (
-          <Main key={key} {...props} style={props}>
-            <Title>
-              <h2>
-                {contentList[item]?.name + '  '}
-                <BiMeteor />
-              </h2>
-            </Title>
+      <Main>
+        <Title>
+          <h2>{contentList[active]?.name + '  '}</h2>
+          <GiStarsStack />
+        </Title>
 
-            <Link>
-              {contentList[item].links?.map((link, index) => {
-                return (
-                  <Button
-                    key={index}
-                    as="a"
-                    href={link.src}
-                    colorHover={true}
-                    active={true}
-                    color={' var(--text-primary)'}
-                    target="blank"
-                  >
-                    {<AiOutlineLink />}
-                    {link.name + ' '}
-                  </Button>
-                );
-              })}
-            </Link>
-            <Image>
-              <img src={contentList[item]?.picture} />
-              <Text>
-                <p>{contentList[item]?.descripition}</p>
-              </Text>
-            </Image>
-          </Main>
-        );
-      })}
+        <Link>
+          {contentList[active].links &&
+            contentList[active].links.map((link, index) => {
+              return (
+                <Button
+                  key={index}
+                  as="a"
+                  href={link.src}
+                  colorHover={true}
+                  active={true}
+                  color={' var(--text-primary)'}
+                  target="blank"
+                >
+                  {<AiOutlineLink />}
+                  {link.name + ' '}
+                </Button>
+              );
+            })}
+        </Link>
+        <Image>
+          <img src={contentList[active]?.picture} alt="icone" />
+          <Text>
+            <p>{contentList[active]?.descripition}</p>
+          </Text>
+        </Image>
+      </Main>
     </>
   );
 };
