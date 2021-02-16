36a41b74556781ca43ce0b31e2748a5759e228f9
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-16 16:16:24 -0300

    feat: new component import and remotion of unnecessary files

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,75 +1,14 @@
-import { useState } from 'react';
-import { Spring } from 'react-spring/renderprops';
-import { Title, Content, Division, Image } from '../../styles/contact';
+import { Container } from '../../styles/contact';
+import Options from './contactOptions';
+import Stars from './starsRain';
 const Contact = () => {
-  const [hover, setHover] = useState(false);
-  const changeHover = () => setHover(!hover);
   return (
-    <Spring
-      from={{ opacity: 0, height: '0%', transform: 'rotateX(90deg)' }}
-      to={{
-        opacity: 1,
-        height: hover ? '90%' : '0%',
-        transform: hover ? 'rotateX(0deg)' : 'rotateX(90deg)',
-      }}
-      config={{ duration: 300 }}
-    >
-      {(props) => (
-        <Content
-          column={true}
-          justify={'center'}
-          onMouseOver={() => changeHover()}
-          onMouseOut={() => changeHover()}
-        >
-          <Title>
-            <label>Onde você pode me encontrar :</label>
-          </Title>
-          <Content
-            width={'100%'}
-            height={'90%'}
-            background={'transparent'}
-            style={props}
-          >
-            <Content
-              width={'100%'}
-              height={'100%'}
-              column={true}
-              justify={'center'}
-            >
-              <Division>
-                <Image src="https://img.icons8.com/color/48/000000/linkedin.png" />
-                <a
-                  href="https://www.linkedin.com/in/alexandre-espejo/"
-                  target="blank"
-                >
-                  Linkedin
-                </a>
-              </Division>
-              <Division>
-                <Image src="https://img.icons8.com/material-sharp/48/000000/github.png" />
-                <a href="https://github.com/alexandreespejo" target="blank">
-                  Github
-                </a>
-              </Division>
-              <Division>
-                <Image src="https://img.icons8.com/fluent/48/000000/instagram-new.png" />
-                <a
-                  href="https://www.instagram.com/alexandre_espejo/"
-                  target="blank"
-                >
-                  Instagram
-                </a>
-              </Division>
-
-              <Division>
-                <Image src="https://img.icons8.com/color/48/000000/gmail--v2.png" />
-                <label>alexandre.espejo@unesp.br</label>
-              </Division>
-            </Content>
-          </Content>
-        </Content>
-      )}
-    </Spring>
+    <>
+      <Container>
+        <Options />
+      </Container>
+      <Stars />
+    </>
   );
 };
 
