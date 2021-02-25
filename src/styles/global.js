d08efa9c80d79b4427d1053da1d6a3a3954c9109
alexandreespejo <alexandreespejo98@gmail.com>
2021-02-24 21:40:52 -0300

    Fix: variable names

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,11 +6,11 @@ export const GlobalStyle = createGlobalStyle`
     --page-secondary:rgba(51, 49, 56, 0.70);
     --text-primary:rgba(250, 250, 250, 0.75);
     --text-secondary:rgba(250, 250, 250, 0.4);
-    --button-primary:rgba(0, 191, 166,0.87);
-    --button-secondary:rgb(0, 210, 180);
+    --green-primary:rgba(0, 191, 166,0.87);
+    --green-secondary:rgba(0, 191, 166,0.2);
   }
   body {
-    font-family:'Roboto',sans-serif;
+    font-family:'Roboto';
     margin: 0;
     padding: 0;
     color: var(--text-primary);
